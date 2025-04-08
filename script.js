a7a6462374fc593b612b9c7cf9b4181f58ada8f9
Tombar42 <40589825@live.napier.ac.uk>
2025-04-08 15:29:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded', async function () {
         return;
     }
 
-    const searchTerm = 'pop';
+    const searchTerm = 'top hits';
     const url = `https://api.spotify.com/v1/search?q=${searchTerm}&type=track&limit=10`;
 
     try {
