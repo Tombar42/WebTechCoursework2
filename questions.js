5b385c2d615ae407b54a60e6ea8345195e81c9c6
Tombar42 <40589825@live.napier.ac.uk>
2025-04-08 10:50:22 +0100

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,43 @@
+//questions.js
+export const questions = [
+  {
+    type: "text",
+    question: "What is the name of the artist who sang 'Shape of You'?",
+    answer: "Ed Sheeran",
+    options: ["Ed Sheeran", "Justin Bieber", "Drake", "Katy Perry"]
+  },
+  {
+    type: "text",
+    question: "Which band is known for the song 'Bohemian Rhapsody'?",
+    answer: "Queen",
+    options: ["Queen", "The Beatles", "Nirvana", "Pink Floyd"]
+  },
+  {
+    type: "text",
+    question: "What year did Michael Jackson release 'Thriller'?",
+    answer: "1982",
+    options: ["1982", "1984", "1980", "1986"]
+  },
+  {
+    type: "text",
+    question: "Who is known as the 'Queen of Pop'?",
+    answer: "Madonna",
+    options: ["Madonna", "Beyoncé", "Lady Gaga", "Taylor Swift"]
+  },
+  {
+    type: "audio",
+    query: "Imagine Dragons Believer"
+  },
+  {
+    type: "audio",
+    query: "Taylor Swift Shake It Off"
+  },
+  {
+    type: "audio",
+    query: "Drake God's Plan"
+  },
+  {
+    type: "audio",
+    query: "The Weeknd Blinding Lights"
+  }
+];
